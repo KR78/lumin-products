0ff51d1547008b9d6759f5f52055eb7f8cb5e60c
Victor Karanja <vic@vappsio.com>
2021-10-28 18:37:11 +0300

    adding in font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,17 @@ import React from 'react';
 import Head from 'next/head';
 import Layout from '../components/layout/AppLayout';
 import '../scss/globals.scss';
+
 const App = ({
   Component,
   pageProps,
 }) => (
   <>
     <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap"
+        rel="stylesheet"
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <Layout>
