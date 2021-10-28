385b4be2bd05b072333a38bfd68cb22b786dbf37
Victor Karanja <vic@vappsio.com>
2021-10-28 20:37:26 +0300

    init siteLanguages helper

---
+++ src/helpers/siteLanguages.ts
@@ -0,0 +1,56 @@
+const languages = [
+  {
+    label: 'AR',
+    value: 'ar',
+  },
+  {
+    label: 'FR',
+    value: 'fr',
+  },
+  {
+    label: 'EN',
+    value: 'en',
+  },
+  {
+    label: 'ES',
+    value: 'es',
+  },
+  {
+    label: 'DE',
+    value: 'de',
+  },
+  {
+    label: 'HE',
+    value: 'he',
+  },
+  {
+    label: 'ID',
+    value: 'id',
+  },
+  {
+    label: 'ZH-TW',
+    value: 'zh-tw',
+  },
+  {
+    label: 'PT',
+    value: 'pt',
+  },
+  {
+    label: 'TH',
+    value: 'th',
+  },
+  {
+    label: 'DA',
+    value: 'da',
+  },
+  {
+    label: 'JA',
+    value: 'ja',
+  },
+  {
+    label: 'KO',
+    value: 'ko',
+  },
+];
+
+export default languages;
