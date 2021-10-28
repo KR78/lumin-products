24ff072491d441f969d1e114e2207dddecedc491
Victor Karanja <vic@vappsio.com>
2021-10-28 22:17:33 +0300

    init Prodcts Section component

---
+++ src/components/layout/Home/Components/ProductsSection/ProductsSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './ProductsSection.module.scss';
+
+const ProductsSection = () => (
+  <article className={style.wrapper}>
+    <section className={style.section}>
+      products
+    </section>
+  </article>
+);
+
+export default ProductsSection;
