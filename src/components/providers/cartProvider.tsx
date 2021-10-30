6dcad810ee78341547ff2361c152ad3c0cbd93b2
Victor Karanja <vic@vappsio.com>
2021-10-30 08:17:51 +0300

    padding in reducer functions and cart states to cart provider

--- src/components/providers/cartProvider.tsx
+++ src/components/providers/cartProvider.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Products } from 'src/types';
+import { Product, Products } from 'src/types';
+import { shopReducer, ADD_PRODUCT, REMOVE_PRODUCT } from 'src/reducers/cart';
 
 interface CartContextProps {
   cart: Products,
-  setCart(v: []): void,
   isCartOpen: boolean,
+  cartLoading: boolean,
+  cartItemsCount: number,
+  addProductToCart(v: Product): void,
   setIsCartOpen(v: boolean): void,
+  setCartLoading(v: boolean): void,
+  removeProductFromCart(v: number): void
 }
 
 export const CartContext = React.createContext({} as CartContextProps);
@@ -17,16 +22,38 @@ interface CartProviderProps {
 const CartProvider = ({
   children,
 }: CartProviderProps) => {
-  const [cart, setCart] = React.useState([]);
   const [isCartOpen, setIsCartOpen] = React.useState(false);
+  const [cartLoading, setCartLoading] = React.useState(false);
+
+  const [cartState, dispatch] = React.useReducer(shopReducer, { cart: [] });
+
+  const addProductToCart = (product) => {
+    setTimeout(() => {
+      dispatch({ type: ADD_PRODUCT, product });
+    }, 300);
+  };
+
+  const removeProductFromCart = (productId) => {
+    setTimeout(() => {
+      dispatch({ type: REMOVE_PRODUCT, productId });
+    }, 300);
+  };
+
+  const cart = cartState?.cart || [];
+
+  const cartItemsCount = (cart || [])?.reduce((count, item) => count + (item?.quantity || 0), 0);
 
   return (
     <CartContext.Provider
       value={{
         cart,
         isCartOpen,
-        setCart,
+        cartItemsCount,
         setIsCartOpen,
+        cartLoading,
+        setCartLoading,
+        addProductToCart,
+        removeProductFromCart,
       }}
     >
       {children}
